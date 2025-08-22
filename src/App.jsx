418b9756da7caf2b973910c864edd79028fe3abf
Mohammad Zeeshan <122723489+CoDx-ZeesH@users.noreply.github.com>
2025-08-22 12:59:00 +0530

    Starting files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+     <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400">
+        🚀 OptiFi with TailwindCSS is working!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
