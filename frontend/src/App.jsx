c5559ee9c0d9aa76cca0d2b4f97cbfa6264bfc64
Mohammad Zeeshan <122723489+CoDx-ZeesH@users.noreply.github.com>
2025-09-22 19:22:34 +0530

    directroy

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginSignup from './components/LoginSignup/LoginSignup';
+import './App.css';
+function App() {
+
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  )
+}
+
+export default App
